16f9732a51850a91bca8e37e2787ae7d8f6c4856
Bada Taejun SeolseongKim <Cirakuze@gmail.com>
2016-06-27 23:59:04 -0400

    Adjusts bass oscillators' durations

--- frontend/util/Bass.js
+++ frontend/util/Bass.js
@@ -24,21 +24,21 @@ Bass.prototype.setup = function() {
 Bass.prototype.trigger = function(time) {
   this.setup();
 
-  this.gain1.gain.setValueAtTime(1, time);
+  this.gain1.gain.setValueAtTime(0.8, time);
   this.gain1.gain.exponentialRampToValueAtTime(0.001, time + 0.5);
   this.osc.frequency.setValueAtTime(60, time);
   this.osc.frequency.exponentialRampToValueAtTime(0.001, time + 0.5);
 
-  this.gain2.gain.setValueAtTime(1, time);
-  this.gain2.gain.exponentialRampToValueAtTime(0.001, time + 0.5);
+  this.gain2.gain.setValueAtTime(0.75, time);
+  this.gain2.gain.exponentialRampToValueAtTime(0.001, time + 0.55);
   this.osc2.frequency.setValueAtTime(50, time);
-  this.osc2.frequency.exponentialRampToValueAtTime(0.001, time + 0.5);
+  this.osc2.frequency.exponentialRampToValueAtTime(0.001, time + 0.55);
 
   this.osc.start(time);
   this.osc.stop(time + 0.5);
 
   this.osc2.start(time);
-  this.osc2.stop(time + 0.5);
+  this.osc2.stop(time + 0.55);
 };
 
 module.exports = Bass;
